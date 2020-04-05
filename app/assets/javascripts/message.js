f8d356439e8ff1ccb76e96e78d91fc9df504c1b3
sam-ken <bvgvc33538@yahoo.co.jp>
2020-04-05 13:36:57 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -77,7 +77,6 @@ $(function(){
         data: {id: last_message_id}
       })
       .done(function(messages) {
-        console.log(messages)
         if (messages.length !== 0) {
           //追加するHTMLの入れ物を作る
           var insertHTML = '';
